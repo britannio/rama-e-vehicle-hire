824dc65f4f73ece804409e5b83c4798b50c9dacc
Britannio <britanniojarrett@gmail.com>
2023-09-07 21:11:55 +0100

    create vehicle update unit test

--- src/test/java/org/example/EVClientTest.java
+++ src/test/java/org/example/EVClientTest.java
@@ -1,12 +1,16 @@
 package org.example;
 
-import com.rpl.rama.Depot;
 import com.rpl.rama.Path;
 import com.rpl.rama.test.InProcessCluster;
 import com.rpl.rama.test.LaunchConfig;
 import junit.framework.TestCase;
+import org.example.data.LatLng;
+
+import java.util.Map;
 
 public class EVClientTest extends TestCase {
+
+  private static final String moduleName = EVModule.class.getName();
 
 
   public void testCreateVehicle() throws Exception {
@@ -25,7 +29,27 @@ public class EVClientTest extends TestCase {
     }
   }
 
-  public void testUpdateVehicle() {
+  private EVClient launchModule(InProcessCluster ipc) {
+    var module = new EVModule();
+    ipc.launchModule(module, new LaunchConfig(1, 1));
+    return new EVClient(ipc);
+  }
+
+  public void testUpdateVehicle() throws Exception {
+    // Changes to the location and battery of a vehicle should be reflected in the $$vehicles PState
+    try (InProcessCluster ipc = InProcessCluster.create()) {
+      var client = launchModule(ipc);
+      var vehicles = ipc.clusterPState(moduleName, "$$vehicles");
+
+      var vehicleId = client.createVehicle();
+      var location = new LatLng(1L, 2L);
+      var battery = 50;
+      client.updateVehicle(vehicleId, battery, location);
+
+      Map<String, Object> vehicleMap = vehicles.selectOne(Path.key(vehicleId));
+      assertEquals(battery, vehicleMap.get("battery"));
+      assertEquals(location, vehicleMap.get("location"));
+    }
   }
 
   public void testCreateAccount() {
